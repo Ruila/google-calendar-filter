ba16b75bf0cdcfe41f772167a5154f0096706d56
Ruila <4momolmr@gmail.com>
2022-04-10 21:24:13 +0800

    move api og getting token to component level

--- src/components/demo/DemoView.tsx
+++ src/components/demo/DemoView.tsx
@@ -1,9 +1,22 @@
 import React, { useEffect } from "react"
 import { ApiContext } from "../../axios/ApiContext"
+import { useAsync } from "react-use"
 
 export const DemoView: React.FunctionComponent = () => {
+  useAsync(async () => {
+    const res =
+      await ApiContext.GoogleCalendarFilterExecutor.getGoogleAccessToken(
+        ApiContext.getClientId(),
+        ApiContext.getAuthCode(),
+        ApiContext.getClientSecret()
+      )
+    ApiContext.setAccessToken(res.data.access_token)
+    return res
+  }, [])
+
   useEffect(() => {
     console.info("axa", ApiContext.getAuthCode())
   }, [])
+
   return <div>113</div>
 }
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,19 +1,7 @@
 import * as React from "react"
 import { DemoView } from "../components/demo/DemoView"
 import { Layout } from "../components/Layout"
-import { ApiContext } from "../axios/ApiContext"
-import { useAsync } from "react-use"
 const DemoPage = () => {
-  useAsync(async () => {
-    const res =
-      await ApiContext.GoogleCalendarFilterExecutor.getGoogleAccessToken(
-        ApiContext.getClientId(),
-        ApiContext.getAuthCode(),
-        ApiContext.getClientSecret()
-      )
-    ApiContext.setAccessToken(res.data.access_token)
-    return res
-  }, [])
   return (
     <Layout>
       <DemoView />
